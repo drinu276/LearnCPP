b59c9c1ece374f25249028c5da6c291ed7c7f67c
andreaa <andrea.attard@gmail.com>
2016-02-22 05:22:26 +0100

    Functions test
    printThis

--- main.cpp
+++ main.cpp
@@ -2,11 +2,16 @@
 
 using namespace std;
 
+void printThis(int a) {
+    cout << a << endl;
+}
+
 int main()
 {
     int x = 0;
     cout << "Enter a value: ";
     cin >> x;
-    cout << "The value entered is: " << x << endl;
+    cout << "The value entered is: ";
+    printThis(x);
     return 0;
 }
